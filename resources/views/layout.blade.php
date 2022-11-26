9bcb813b949df582aa0563fa4baf26c523511ef5
yan043 <mahdian043@gmail.com>
2022-11-26 11:18:00 +0800

    add layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -162,8 +162,8 @@
       </div>
       <!-- /.content-wrapper -->
       <footer class="main-footer">
-        <div class="float-right d-none d-sm-block"><b>Version</b> 3.0.5</div>
-        <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+        <div class="float-right d-none d-sm-block"><b>Version</b> 1.0.x</div>
+        <strong>Copyright &copy; {{ date('Y') }} Monitoring Access Logistics SO | POLIBAN.</strong> All rights reserved.
       </footer>
 
       <!-- Control Sidebar -->
